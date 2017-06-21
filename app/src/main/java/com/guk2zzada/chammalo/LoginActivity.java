7e3d650ab0de4700ff352e0f5465791cae290037
CPstudy <guide9411@gmail.com>
2017-06-21 18:01:45 +0900

    Update LoginActivity.java

--- app/src/main/java/com/guk2zzada/chammalo/LoginActivity.java
+++ app/src/main/java/com/guk2zzada/chammalo/LoginActivity.java
@@ -57,6 +57,7 @@ public class LoginActivity extends AppCompatActivity {
         ckbID = (CheckBox) findViewById(R.id.ckbID);
         ckbPW = (CheckBox) findViewById(R.id.ckbPW);
 
+        // 네트워크 통신을 위해 필요한 것
         StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
         StrictMode.setThreadPolicy(policy);
 
@@ -65,6 +66,7 @@ public class LoginActivity extends AppCompatActivity {
         strID = mPref.getString("strID", "");
         strPW = mPref.getString("strPW", "");
 
+        // 아이디 저장과 패스워드 저장 체크박스 초기화
         if(boolID) {
             ckbID.setChecked(true);
             edtID.setText(strID);
@@ -79,6 +81,7 @@ public class LoginActivity extends AppCompatActivity {
             ckbPW.setChecked(false);
         }
 
+        // 회원가입 버튼
         btnJoin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -87,26 +90,19 @@ public class LoginActivity extends AppCompatActivity {
             }
         });
 
+        // 
         btnSign.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 String result = "0";
 
-                String ID = "2mb";
-                String PW = "1234";
-
+                // 입력 받은 아이디와 패스워드
                 String postID = edtID.getText().toString();
                 String postPW = edtPW.getText().toString();
 
                 if(!edtID.getText().toString().equals("") && !edtPW.getText().toString().equals("")) {
-                    /*if (edtID.getText().toString().equals(ID) && edtPW.getText().toString().equals(PW)) {
-                        Intent intent = new Intent(getApplicationContext(), MainActivity.class);
-                        startActivity(intent);
-                    } else {
-                        showToast("아이디나 패스워드가 맞지 않습니다.");
-                    }*/
-
                     try {
+                        // 아래 URL에 접속해 JSON 배열을 받아와 파싱함
                         URL url = new URL("http://sunsiri.cafe24.com/login-android.jsp");
                         HttpURLConnection httpURLcon = (HttpURLConnection)url.openConnection();
                         httpURLcon.setDefaultUseCaches(false);
@@ -127,16 +123,8 @@ public class LoginActivity extends AppCompatActivity {
                             result = line;
                         }
 
-                        // 서버에서 받아온 결과문을 확인합니다.
+                        // 서버에서 받아온 결과문을 확인. 1 = 성공
                         System.out.println(result);
-
-                        XmlPullParserFactory factory = XmlPullParserFactory.newInstance();
-                        XmlPullParser parser = factory.newPullParser();
-
-                        InputStream input = new ByteArrayInputStream(result.getBytes("UTF-8"));
-                        parser.setInput(input, "UTF-8");
-
-                        int EventType = parser.getEventType();
 
                     } catch (Exception e) {
                         e.printStackTrace();
@@ -159,6 +147,7 @@ public class LoginActivity extends AppCompatActivity {
                         }
                         mPrefEdit.commit();
 
+                        // 아이디와 비밀번호가 확인되면 MainActivity로 이동하면서 사용자 아이디를 
                         Intent intent = new Intent(getApplicationContext(), MainActivity.class);
                         intent.putExtra("strID", postID);
                         startActivity(intent);
